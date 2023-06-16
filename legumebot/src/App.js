416f7964c63c8a1f5e80aa4677e4b1777de13192
Sam Hokin <shokin@ncgr.org>
2023-06-16 07:53:50 -0600

    Use .env for OpenAI model.

--- legumebot/src/App.js
+++ legumebot/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-              <h1 className="App-header">Legumebot! (Experimental OpenAI chatbot on GPT-3.5)</h1>
+		<h1 className="App-header">Legumebot! (Experimental OpenAI chatbot on { process.env.REACT_APP_OPENAI_MODEL })</h1>
               <p className="App-intro">
                 This is an <b>experimental</b> chatbot for answering questions about legume genera, with content from PubMed and PubAg publication abstracts.
                 I built the Pinecone vector database from PubMed/PubAg abstracts that contain a legume genus and species name.
